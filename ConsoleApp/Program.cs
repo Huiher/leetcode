03ed5fec87e95f52fdabb0c9af180c09699d8b20
huihe7 <huiher@gmail.com>
2022-08-25 12:19:57 +0100

    first commit and push

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace VSCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = TwoSum(new int[]{1,2,3}, 4);
+
+            if (result.Length > 0)
+            {
+                Console.WriteLine(result[0]);
+                Console.WriteLine(result[1]);
+            }
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for(int i=0; i<nums.Length-1; i++)
+            {
+                for(int j=1+i; j<nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        Console.WriteLine(i);
+                        Console.WriteLine(j);
+
+                        return new int[2]{i, j};
+                    }
+                }
+            }
+
+            return new int[0];
+        }
+
+        public int Foo()
+        {
+            return 1;
+        }
+    }
+}
