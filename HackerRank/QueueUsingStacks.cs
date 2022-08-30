77515321ca70a249c572d5acabde889b810284cc
huihe7 <huiher@gmail.com>
2022-08-30 11:56:51 +0100

    HR: [WIP] queue using stacks

--- HackerRank/QueueUsingStacks.cs
+++ HackerRank/QueueUsingStacks.cs
@@ -5,6 +5,29 @@ using System.Threading.Tasks;
 
 namespace HackerRank
 {
+    class StackQueue{
+        
+        private Stack<int> _enqueueStack;
+        private Stack<int> _dequeueStack;
+        
+        public StackQueue()
+        {
+            _enqueueStack = new Stack<int>();
+            _dequeueStack = new Stack<int>();
+            
+        }
+        
+        // Enqueue a element
+        public void Enqueue(int n){
+            _enqueueStack.Push(n);           
+        }
+        
+        public int Dequeue(){
+            // Get from the bottom of the 'stack'
+            return 0;
+        }
+    }
+
     public class QueueUsingStacks
     {
         
